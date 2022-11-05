9675d4645950ca9f1d031db2f0a1057f7fe9253b
irvanln <irvina22@gmail.com>
2022-11-05 20:21:49 +0700

    method overloading

---
+++ MethodOverloading/src/methodoverloading/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package methodoverloading;
+
+/**
+ *
+ * @author Irv-PC
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          var employee = new Employee(50000, 20);
+        int wage = employee.calculateWage();
+        System.out.println(wage);
+    }
+    
+}
